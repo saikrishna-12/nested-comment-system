fa8f74d65706c5a8844e28028ce5a4928b2ae994
saikrishna-12 <saikrishnasai12@gmail.com>
2023-11-02 06:42:33 +0530

    dummy values added

--- src/Store/commentReducer.js
+++ src/Store/commentReducer.js
@@ -1,6 +1,21 @@
 const initialState = {
-    comments: [],
-    child:[],
+    comments: [
+        { id: 1, commentMessage: 'Dummy Comment 1' },
+        { id: 2, commentMessage: 'Dummy Comment 2' },
+        { id: 3, commentMessage: 'Dummy Comment 3' },
+        { id: 4, commentMessage: 'Dummy Comment 4' },
+        { id: 5, commentMessage: 'Dummy Comment 5' },
+        { id: 6, commentMessage: 'Dummy Comment 6' },
+        { id: 7, commentMessage: 'Dummy Comment 7' },
+        { id: 8, commentMessage: 'Dummy Comment 8' },
+        { id: 9, commentMessage: 'Dummy Comment 9' },
+        { id: 10, commentMessage: 'Dummy Comment 10' },
+      ],
+      child: [
+        { id: 20, commentMessage: 'Dummy Reply 1', parent: 1 },
+        { id: 21, commentMessage: 'Dummy Reply 2', parent: 2 },
+        
+      ],
   };
   
   const commentReducer = (state = initialState, action) => {
